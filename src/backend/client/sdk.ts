469c963ba47d52626e9e55278b110ab32c47be27
MyManAy <Nithin.monni@gmail.com>
2022-12-23 20:41:01 -0600

    realm sdk connection with gql client

--- src/backend/client/sdk.ts
+++ src/backend/client/sdk.ts
@@ -1,10 +1,28 @@
 import { GraphQLClient } from "graphql-request";
+import * as Realm from "realm-web";
 
 const endpoint = "https://money.hasura.app/v1/graphql";
 
-export const client = new GraphQLClient(endpoint, {
-  headers: {
-    "x-hasura-admin-secret":
-      "PEXc9q6lySrzh2IeMUQTlD9gDG4tt39Qnx721QarVJYAhSUfkXUNSD27xSwG76pt",
-  },
+// Connect to your MongoDB Realm app
+const app = new Realm.App("application-0-weajw");
+// Gets a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  // Guarantee that there's a logged in user with a valid access token
+  if (!app.currentUser) {
+    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
+    // access token.
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. Tokens must be refreshed after
+    // 30 minutes. To guarantee that the token is valid, we refresh the user's access token.
+    await app.currentUser.refreshAccessToken();
+  }
+  return app.currentUser!.accessToken;
+}
+
+export const sdk = new GraphQLClient(endpoint, {
+  headers: (async () => {
+    const token = await getValidAccessToken();
+    return { Authorization: `Bearer ${token}` };
+  }) as any,
 });
