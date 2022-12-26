a1086bd8fba01ef6833883e4ff40172f56a544bd
MyManAy <Nithin.monni@gmail.com>
2022-12-25 20:27:30 -0600

    login functionality

--- src/figmaComponents/LoginButton.tsx
+++ src/figmaComponents/LoginButton.tsx
@@ -1,10 +1,24 @@
 import { FunctionComponent } from "react";
 import styles from "./LoginButton.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton: FunctionComponent = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   return (
-    <button className={styles.loginButton}>
-      <div className={styles.h6}>Login</div>
+    <button
+      className={styles.loginButton}
+      onClick={() => {
+        isAuthenticated
+          ? logout({
+              returnTo: "http://localhost:3000/MyManAy.github.io-Volunteering/",
+            })
+          : loginWithRedirect({
+              redirectUri:
+                "http://localhost:3000/MyManAy.github.io-Volunteering/",
+            });
+      }}
+    >
+      <div className={styles.h6}> {isAuthenticated ? "Logout" : "Login"}</div>
     </button>
   );
 };
--- src/figmaComponents/NavigationBar.tsx
+++ src/figmaComponents/NavigationBar.tsx
@@ -1,13 +1,21 @@
 import { FunctionComponent } from "react";
-import LoginButton from "../components/LoginButton";
+import LoginButton from "./LoginButton";
 import styles from "./NavigationBar.module.css";
+import defaultProfile from "../images/profile.svg";
+import logo from "../images/logo@2x.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const NavigationBar: FunctionComponent = () => {
+  const { isAuthenticated, user } = useAuth0();
   return (
     <nav className={styles.navigationBar}>
-      <img className={styles.profileIcon} alt="" src="../profile.svg" />
+      <img
+        className={styles.profileIcon}
+        alt=""
+        src={isAuthenticated ? user?.picture : defaultProfile}
+      />
       <LoginButton />
-      <img className={styles.logoIcon} alt="" src="../logo@2x.png" />
+      <img className={styles.logoIcon} alt="" src={logo} />
     </nav>
   );
 };
