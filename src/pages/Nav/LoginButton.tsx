fab0c8ce47b7c68b916f2c02a24c475d2b261d7d
nithin monni <nithin.monni@gmail.com>
2023-02-08 19:28:32 -0600

    changed the redirect link

--- src/pages/Nav/LoginButton.tsx
+++ src/pages/Nav/LoginButton.tsx
@@ -4,7 +4,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton: FunctionComponent = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-  const redirectLink = "http://localhost:3000/MyManAy.github.io-Volunteering/";
+  const redirectLink =
+    "https://mymanay.github.io/MyManAy.github.io-Volunteering/";
   return (
     <button
       className={styles.loginButton}
